55ba8b9fad46ece63291d4f81b0f82470390f0a4
Phuong Huynh <huynhpt@gmail.com>
2017-12-15 10:08:27 +1100

    Changed from prismic-javascript to prismic.io

--- src/fetch.js
+++ src/fetch.js
@@ -1,21 +1,21 @@
-import Prismic from 'prismic-javascript'
+import Prismic from 'prismic.io';
 
 module.exports = async ({ repositoryName, accessToken }) => {
-  console.time(`Fetch Prismic data`)
-  console.log(`Starting to fetch data from Prismic`)
+  console.time(`Fetch Prismic data`);
+  console.log(`Starting to fetch data from Prismic`);
 
-  const apiEndpoint = `https://${repositoryName}.prismic.io/api/v2`
-  const client = await Prismic.getApi(apiEndpoint, { accessToken })
+  const apiEndpoint = `https://${repositoryName}.prismic.io/api`;
+  const client = await Prismic.api(apiEndpoint, { accessToken });
 
   // Query all documents from client
-  const documents = await pagedGet(client)
-
-  console.timeEnd(`Fetch Prismic data`)
+  const documents = await pagedGet(client);
+  console.log('docs');
+  console.timeEnd(`Fetch Prismic data`);
 
   return {
-    documents,
-  }
-}
+    documents
+  };
+};
 
 async function pagedGet(
   client,
@@ -27,26 +27,33 @@ async function pagedGet(
 ) {
   const mergedOptions = Object.assign(
     {
-      lang: '*',
+      lang: '*'
     },
     options
-  )
+  );
 
   const response = await client.query(query, {
     ...mergedOptions,
     page,
-    pageSize,
-  })
+    pageSize
+  });
 
   if (!aggregatedResponse) {
-    aggregatedResponse = response.results
+    aggregatedResponse = response.results;
   } else {
-    aggregatedResponse = aggregatedResponse.concat(response.results)
+    aggregatedResponse = aggregatedResponse.concat(response.results);
   }
 
   if (page * pageSize < response.total_results_size) {
-    return pagedGet(client, query, options, page + 1, pageSize, aggregatedResponse)
+    return pagedGet(
+      client,
+      query,
+      options,
+      page + 1,
+      pageSize,
+      aggregatedResponse
+    );
   }
 
-  return aggregatedResponse
+  return aggregatedResponse;
 }
--- src/nodes.js
+++ src/nodes.js
@@ -1,28 +1,31 @@
-import { camelCase, upperFirst } from 'lodash'
-import { createHash } from 'crypto'
-import stringify from 'json-stringify-safe'
+import { camelCase, upperFirst } from 'lodash';
+import { createHash } from 'crypto';
+import stringify from 'json-stringify-safe';
 
-const sourceId = `__SOURCE__`
-const typePrefix = `Prismic`
-const conflictFieldPrefix = `prismic`
-const restrictedNodeFields = [`id`, `children`, `parent`, `fields`, `internal`]
+const sourceId = `__SOURCE__`;
+const typePrefix = `Prismic`;
+const conflictFieldPrefix = `prismic`;
+const restrictedNodeFields = [`id`, `children`, `parent`, `fields`, `internal`];
 
-const makeTypeName = type => upperFirst(camelCase(`${typePrefix} ${type}`))
-const digest = str => createHash(`md5`).update(str).digest(`hex`)
+const makeTypeName = type => upperFirst(camelCase(`${typePrefix} ${type}`));
+const digest = str =>
+  createHash(`md5`)
+    .update(str)
+    .digest(`hex`);
 
 // DocumentNode
 //
 // Each document is a DocumentNode. Data is available in the `data` property.
 export const DocumentNode = ({ documentItem: documentItemOriginal }) => {
-  const documentItem = Object.assign({}, documentItemOriginal)
+  const documentItem = Object.assign({}, documentItemOriginal);
 
   // Prefix conflicting keys.
   Object.keys(documentItem).forEach(key => {
     if (restrictedNodeFields.includes(key)) {
-      documentItem[conflictFieldPrefix + upperFirst(key)] = documentItem[key]
-      delete documentItem[key]
+      documentItem[conflictFieldPrefix + upperFirst(key)] = documentItem[key];
+      delete documentItem[key];
     }
-  })
+  });
 
   // Need to use prismicId since the original id key conflicts with Gatsby.
   const node = {
@@ -31,11 +34,11 @@ export const DocumentNode = ({ documentItem: documentItemOriginal }) => {
     parent: sourceId,
     children: [],
     internal: {
-      type: makeTypeName(`Document`),
-    },
-  }
+      type: makeTypeName(`Document`)
+    }
+  };
 
-  node.internal.contentDigest = digest(stringify(node))
+  node.internal.contentDigest = digest(stringify(node));
 
-  return node
-}
+  return node;
+};
